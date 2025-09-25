0ae9b8410b04009356c79862aa20fd08d09761e0
mhsobuz <mahmudsobuz554@gmail.com>
2025-09-26 02:42:23 +0600

    Initial commit for flutter Tutorials/lib

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text("Hello World_2"))),
+    );
+  }
+}
