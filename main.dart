0763c9d93ac39e9cc9ce0d0f01a1b959d2700fb9
mhsobuz <mahmudsobuz554@gmail.com>
2025-09-26 08:28:53 +0600

    Add Card with elevation

--- main.dart
+++ main.dart
@@ -11,11 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Image.network(
-              "https://windflowerflorist.com/cdn/shop/articles/unnamed-_16_a3c38aff-bd63-4976-86c4-8b644de5e484_1600x.jpg",
+        body: Container(
+          color: Colors.blueGrey,
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Card(
+                elevation: 20,
+                child: Image.network(
+                  "https://windflowerflorist.com/cdn/shop/articles/unnamed-_16_a3c38aff-bd63-4976-86c4-8b644de5e484_1600x.jpg",
+                ),
+              ),
             ),
           ),
         ),
