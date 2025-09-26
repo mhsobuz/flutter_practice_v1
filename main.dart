8308a831aa5e8699ee95ba0c32624b229740ca26
mhsobuz <mahmudsobuz554@gmail.com>
2025-09-27 02:35:39 +0600

    ListView: widget extension

--- main.dart
+++ main.dart
@@ -17,58 +17,28 @@ class MyApp extends StatelessWidget {
           width: double.infinity,
           child: ListView(
             children: [
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ),
+              ListItem(),
+              ListItem(),
+              ListItem(),
+              ListItem(),
+              ListItem(),
+              ListItem(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  const ListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Container(width: double.infinity, height: 200, color: Colors.red),
     );
   }
 }
